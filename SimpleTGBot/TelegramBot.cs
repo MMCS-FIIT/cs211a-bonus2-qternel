9efc22177e338633d7ad05479780cd3dff401fd1
Michael Sidorchenko <qternel@gmail.com>
2024-05-08 16:24:51 +0300

    temporary commit

--- SimpleTGBot/TelegramBot.cs
+++ SimpleTGBot/TelegramBot.cs
@@ -1,17 +1,16 @@
-﻿using System.Reflection.Metadata.Ecma335;
-
-namespace SimpleTGBot;
+﻿namespace SimpleTGBot;
 using Telegram.Bot;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
-
+using Telegram.Bot.Types.ReplyMarkups;
+using Utils;
 public class TelegramBot
 {
     // Токен TG-бота. Можно получить у @BotFather
-    private const string BotToken = "ВАШ_ТОКЕН_ИДЕНТИФИКАЦИИ_БОТА";
-    
+    private const string BotToken = "7100637035:AAHuRxdCBzQOuZXASiS24Mb7dMH61InBdBo";
+
     /// <summary>
     /// Инициализирует и обеспечивает работу бота до нажатия клавиши Esc
     /// </summary>
@@ -20,18 +19,19 @@ public class TelegramBot
         // Если вам нужно хранить какие-то данные во время работы бота (массив информации, логи бота,
         // историю сообщений для каждого пользователя), то это всё надо инициализировать в этом методе.
         // TODO: Инициализация необходимых полей
-        
+
+
         // Инициализируем наш клиент, передавая ему токен.
         var botClient = new TelegramBotClient(BotToken);
-        
+
         // Служебные вещи для организации правильной работы с потоками
         using CancellationTokenSource cts = new CancellationTokenSource();
-        
+
         // Разрешённые события, которые будет получать и обрабатывать наш бот.
         // Будем получать только сообщения. При желании можно поработать с другими событиями.
         ReceiverOptions receiverOptions = new ReceiverOptions()
         {
-            AllowedUpdates = new [] { UpdateType.Message }
+            AllowedUpdates = new[] { UpdateType.Message }
         };
 
         // Привязываем все обработчики и начинаем принимать сообщения для бота
@@ -45,14 +45,14 @@ public class TelegramBot
         // Проверяем что токен верный и получаем информацию о боте
         var me = await botClient.GetMeAsync(cancellationToken: cts.Token);
         Console.WriteLine($"Бот @{me.Username} запущен.\nДля остановки нажмите клавишу Esc...");
-        
+
         // Ждём, пока будет нажата клавиша Esc, тогда завершаем работу бота
-        while (Console.ReadKey().Key != ConsoleKey.Escape){}
+        while (Console.ReadKey().Key != ConsoleKey.Escape) { }
 
         // Отправляем запрос для остановки работы клиента.
         cts.Cancel();
     }
-    
+
     /// <summary>
     /// Обработчик события получения сообщения.
     /// </summary>
@@ -61,36 +61,53 @@ public class TelegramBot
     /// <param name="cancellationToken">Служебный токен для работы с многопоточностью</param>
     async Task OnMessageReceived(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
     {
-        // Работаем только с сообщениями. Остальные события игнорируем
+
+        ReplyKeyboardMarkup replyKeyboardMarkup = new(new[]
+        {
+        new KeyboardButton[] { "123" },
+         })
+        {
+            ResizeKeyboard = true
+        };
+
+
         var message = update.Message;
         if (message is null)
         {
             return;
         }
-        // Будем обрабатывать только текстовые сообщения.
-        // При желании можно обрабатывать стикеры, фото, голосовые и т. д.
-        //
-        // Обратите внимание на использованную конструкцию. Она эквивалентна проверке на null, приведённой выше.
-        // Подробнее об этом синтаксисе: https://medium.com/@mattkenefick/snippets-in-c-more-ways-to-check-for-null-4eb735594c09
-        if (message.Text is not { } messageText)
+
+
+        //  Utils.Utils.PrintData(message.Text);
+
+        /* if (message.Text is not { } messageText) // if is null
+         {
+             Console.WriteLine($"{message.Type}!!!!");
+             return;
+         }*/
+
+        long chatId = message.Chat.Id;
+
+        if (message.Type == MessageType.Text)
         {
-            return;
+            var messageText = message.Text;
+
+            if (messageText.StartsWith("/GetUserInformation"))
+            {
+
+                if (messageText.Split().Length == 1)
+                    return;
+
+                Utils.GetData(messageText.Split()[1]);
+
+
+                /*await Utils.GetUserInformation(botClient, chatId, replyKeyboardMarkup, update, cancellationToken);*/
+            }
+
+
         }
-
-        // Получаем ID чата, в которое пришло сообщение. Полезно, чтобы отличать пользователей друг от друга.
-        var chatId = message.Chat.Id;
-        
-        // Печатаем на консоль факт получения сообщения
-        Console.WriteLine($"Получено сообщение в чате {chatId}: '{messageText}'");
-
-        // TODO: Обработка пришедших сообщений
-        
-        // Отправляем обратно то же сообщение, что и получили
-        Message sentMessage = await botClient.SendTextMessageAsync(
-            chatId: chatId,
-            text: "Ты написал:\n" + messageText,
-            cancellationToken: cancellationToken);
     }
+
 
     /// <summary>
     /// Обработчик исключений, возникших при работе бота
@@ -106,12 +123,12 @@ public class TelegramBot
         {
             ApiRequestException apiRequestException
                 => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
-            
+
             _ => exception.ToString()
         };
 
         Console.WriteLine(errorMessage);
-        
+
         // Завершаем работу
         return Task.CompletedTask;
     }
---
+++ SimpleTGBot/Utils.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using RestSharp;
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+using System.Text.RegularExpressions;
+namespace Utils
+{
+    class Utils
+    {
+
+        public static async void GetData(string handle)
+        {
+
+            await Task.Run(() =>
+            {
+                string url = $"https://codeforces.com/api/user.info?handles={handle}";
+                var client = new RestClient(url);
+                var request = new RestRequest();
+
+                var response = client.Get(request);
+
+                Console.WriteLine(response.Content.ToString());
+                string userFile = $"userData_{handle}.txt";
+                if (System.IO.File.Exists(userFile))
+                    return;
+
+                System.IO.File.WriteAllTextAsync(userFile, response.Content.ToString());
+
+            });
+
+        }
+
+    }
+}
